18dabff44b940e83f9956e898f4c522a08f2495c
smaxad47 <smaxad47@users.noreply.github.com>
2023-06-08 08:51:29 +0000

    chore: update config file

--- static/scripts/config.js
+++ static/scripts/config.js
@@ -1,5 +1,9 @@
 export default {
   items: [
-    "meme/2.png"
+    "meme/2.png",
+    "meme/QQ图片20230608164854.jpg",
+    "meme/QQ图片20230608164932.png",
+    "meme/QQ图片20230608165006.png",
+    "meme/QQ图片20230608165012.jpg"
   ]
 }
